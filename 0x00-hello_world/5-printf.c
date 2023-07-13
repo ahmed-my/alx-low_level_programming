598fa460d0c8a35984ca7ecd8eed1cf062a20af0
ahmed-my <yahaya.mymohammed@gmail.com>
2023-07-13 07:04:14 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -8,6 +8,6 @@
  */
 int main(void)
 {
-	printf("%s%c%s%c", "with proper grammar", ',', " but the outcome is a piece of art", ',');
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
