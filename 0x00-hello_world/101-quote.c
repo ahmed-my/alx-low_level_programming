d86b75ddcb6dfe1590ae753e54525ebc8b60aa46
ahmed-my <yahaya.mymohammed@gmail.com>
2023-07-13 10:42:41 -0700

    Error to standard err

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry of the program
+ * program to print without using puts() or prinf() function
+ * Return: Always 1 (Error)
+ */
+int main(void)
+{
+	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1);
+}
