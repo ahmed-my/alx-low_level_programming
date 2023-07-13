b2a350d5ab197a595c2564884541c44782620bff
ahmed-my <yahaya.mymohammed@gmail.com>
2023-07-13 04:55:14 -0700

    puts command

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,9 +3,9 @@
  * main - Entry point
  *
  * Return Always 0 (success)
-**/
+*/
 int main(void)
 {
-	puts("\nProgramming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
