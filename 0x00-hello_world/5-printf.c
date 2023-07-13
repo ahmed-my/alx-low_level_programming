ecfd958cd240c7dbf9ae42de3dd3fcf2fd056cfe
ahmed-my <yahaya.mymohammed@gmail.com>
2023-07-13 06:56:58 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - entry point for the program
+ *
+ * Description: A program that print the string
+ * with proper grammar, but the outcome is a piece of art,
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("%s%c%s%c", "with proper grammar", ',', " but the outcome is a piece of art", ',');
+	return (0);
+}
