60eece669e9ed0d6583e0f314b51ff5606fdcafb
ahmed-my <yahaya.mymohammed@gmail.com>
2023-07-14 15:18:46 -0700

    Variables_if_else_while

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,12 +9,12 @@ int main(void)
 {
 	int number;
 
-	for (number = 0; number <= 9; number++)
+	for (number = 0; number < 10; number++)
 	{
 		putchar(number);
 		putchar(',');
 		putchar(' ');
+		putchar('\n');
 	}
-	putchar('\n');
 	return (0);
 }
