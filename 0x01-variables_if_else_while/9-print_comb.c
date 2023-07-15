0335d7d73a4942f438166b65ff0b7fb79457399c
ahmed-my <yahaya.mymohammed@gmail.com>
2023-07-14 17:14:51 -0700

    Variables_if_else_while

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,9 +11,9 @@ int main(void)
 
 	for (num = 0; num <= 9; num++)
 	{
-		putchar((num % 10) + '0');
-		putchar(',');
-		putchar(' ');
+		putchar(num + '0');
+		putchar(",");
+		putchar(" ");
 	}
 	putchar('\n');
 	return (0);
