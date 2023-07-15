d6e6c45b04c4a7b9b4a216e02176f762dc30309b
ahmed-my <yahaya.mymohammed@gmail.com>
2023-07-14 20:03:05 -0700

    Variables_if_else_while

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+	int num1;
+	int num2;
+
+	for (num1 = 0; num1 < 100, num1++)
+	{
+		for (num2 = 0; num2 < 100; num2++)
+		{
+			putchar((num1 / 10) + '0');
+			putchar((num1 % 10) + '0');
+			purchar(',');
+			purchar(' ');
+			putchar((num2 / 10) + '0');
+			putchar((num2 % 10) + '0');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
