46a5901c05d50c61d5f59785a2c13d3f3bc97edf
ahmed-my <yahaya.mymohammed@gmail.com>
2023-07-14 17:57:24 -0700

    Variables_if_else_while

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,6 +8,7 @@ int main(void)
 {
 	int num1;
 	int num2;
+
 	for (num1 = 0; num1 < 9; num1+=)
 	{
 		for (num2 = num1 + 1; num2 < 10; num2++)
@@ -15,11 +16,11 @@ int main(void)
 			putchar((num1 % 10) + '0');
 			putchar((num2 % 10) + '0');
 			if (num1 == 8 && num2 == 9)
-				continue;
+				break;
 			putchar(',');
 			putchar(' ');
 		}
 	}
-	putchar('\n');
+        putchar('\n');
 	return (0);
 }
