bfadc92487d638ecf2751bed4d778766e1cae5a7
ahmed-my <yahaya.mymohammed@gmail.com>
2023-07-18 19:04:43 +0000

    Functions and nested loops

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isalpha - A program to determine alphabet
+ *
+ * Return: Success 0
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
