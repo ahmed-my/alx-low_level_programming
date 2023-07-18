b584f2385ba27c9216e160e2a60afffddad6e0d5
ahmed-my <yahaya.mymohammed@gmail.com>
2023-07-18 20:23:50 +0000

    Functions and nested loops

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _abs - Absolute value of a number
+ *
+ * Return: Success 0
+ *
+*/
+
+int _abs(int num)
+{
+	if (num > 0)
+	{
+		return (num);
+	}
+	else
+	{
+		return (-num);
+	}
+}
