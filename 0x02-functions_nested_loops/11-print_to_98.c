533a1b892e5e2aa35f16ec1684a290dd2ef2ee17
ahmed-my <yahaya.mymohammed@gmail.com>
2023-07-19 00:35:30 +0000

    Function and nested loops

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_to_98 - print from n to 98
+ *
+ * Return: Success 0;
+ */
+void print_to_98(int n)
+{
+	if (n >= 98)
+	{
+		while(n > 98)
+		{
+			printf("%d", n--);
+			printf(",");  
+			printf(" ");
+		}  
+		printf("%d\n", n);
+	}
+	else
+	{
+		while(n < 98)
+		{
+			printf("%d", n++);
+			printf(",");
+			printf(" ");
+		}
+		printf("%d\n", n);
+	}
+} 
