92ddbb4e5c8a33ff355bb2fcf4b89beb5f9c90ad
ahmed-my <yahaya.mymohammed@gmail.com>
2023-07-19 21:46:58 +0000

    Functions and nested loops

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	unsigned long fibo1 = 0, fibo2 = 1, fibosum;
+	float total_sum;
+
+	while (1)
+	{
+		fibosum = fibo1 + fibo2;
+
+		if (fibosum > 4000000)
+			break;
+		if ((fibosum % 2) == 0)
+			total_sum += fibosum;
+		fibo1 = fibo2;
+		fibo2 = fibosum;
+
+	}
+	printf("%.0f\n", total_sum);
+	return (0);
+}
