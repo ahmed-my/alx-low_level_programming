9821c097f9144076a47a7f5f599cf291ab783b74
ahmed-my <yahaya.mymohammed@gmail.com>
2023-07-19 22:04:26 +0000

    Functions and nested loops

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -7,12 +7,14 @@
 int main(void)
 {
 	int count;
-	unsigned long fibo1 = 0, fibo2 = 1, sum;
+	unsigned long fibo1 = 0;
+	unsigned long fibo2 = 1;
+	unsigned long sum;
 
 	for (count = 0; count < 50; count++)
 	{
 		sum = fibo1 + fibo2;
-		printf("%lu", fibo1);
+		printf("%lu", sum);
 		fibo1 = fibo2;
 		fibo2 = sum;
 		if (count == 49)
