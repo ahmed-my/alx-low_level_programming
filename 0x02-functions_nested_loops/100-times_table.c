4674b75390f4f39dcdc8f52d2b210ea4845f7f04
ahmed-my <yahaya.mymohammed@gmail.com>
2023-07-20 00:07:11 +0000

    Functions and nested loops

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,46 @@
+#include "main.h"
+/**
+ * print_times_table - prints the times table
+ * @n: The integer to check
+ *
+ * Return: Always 0.
+ */
+void print_times_table(int n)
+{
+	int i, j, product;
+
+	if (n <= 15 && n >= 0)
+	{
+		for (i = 0; i <= n; i++)
+		{
+			_putchar('0');
+			for (j = 1; j <= n; j++)
+			{
+				_putchar(',');
+				_putchar(' ');
+				product = i * j;
+				if (product <= 99)
+				{
+					_putchar(' ');
+					_putchar(product + '0');
+				}
+				if (product >= 100)
+				{
+					_putchar((product / 100) + '0');
+					_putchar((product / 10) % 10 + '0');
+				}
+				if (product <= 9)
+				{
+					_putchar(' ');
+					_putchar(product + '0');
+				}
+				else if (product <= 99 && product >= 10)
+				{
+					_putchar((product / 10) + '0');
+				}
+				_putchar((product % 10) + '0');
+				_putchar('\n');
+			}
+		}
+	}
+}
