b186c05a59f78367f433d5f447c17ba27a6c58f4
ahmed-my <yahaya.mymohammed@gmail.com>
2023-08-30 01:36:46 +0000

    function that reverses a listint_t linked list

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * reverse_listint - reverse a linked list
+ * @head: pointer to the listint_t
+ * Return: pointer to the first node of the reversed list
+ *
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *n = NULL; /* n denote next */
+	listint_t *p = NULL; /* p denote previous */
+
+	while (head)
+	{
+		next = (*head)->next;
+		(*head)->next = p;
+		p = *head;
+		*head = n;
+	}
+	*head = p;
+	return (*head);
+}
