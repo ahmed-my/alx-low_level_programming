b32cb51864e4874902b22e04db04e562791c9c3b
ahmed-my <yahaya.mymohammed@gmail.com>
2023-10-16 16:42:11 +0000

    Dynamic Library

---
+++ 0x18-dynamic_libraries/calculator.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+/**
+ * add - function to add two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: the sum of a + b
+ *
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * sub - function to remove integer b from integer a
+ * @a: first integer
+ * @b: second integer
+ * Return: the diffence a - b
+ *
+ */
+int sub(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * mul - function to multiple two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: the diffence a * b
+ *
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * div - function to divide two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: the diffence a / b
+ *
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * mod - function to get the modulus of two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: the reminder of a / b
+ *
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
