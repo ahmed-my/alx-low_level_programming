ce8886f3348bee0e123bf9440eda4794f8eee8e1
ahmed-my <yahaya.mymohammed@gmail.com>
2023-10-17 14:15:03 +0000

    Dynamic Library

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/ahmed-my/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libgiga.so
-export LD_PRELOAD=/tmp/libgiga.so
+gcc -shared -o my_program.so -fPIC my_program.c
+export LD_PRELOAD=./my_program.so:$LD_LIBRARY_PATH
