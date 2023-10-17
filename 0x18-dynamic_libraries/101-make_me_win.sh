232b9c5a76099c741bcd1e26adfd787621be4420
ahmed-my <yahaya.mymohammed@gmail.com>
2023-10-17 08:40:52 +0000

    Dynamic Library

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,1 +1,3 @@
 #!/bin/bash
+export LD_PRELOAD=/home/vagrant/alx-low_level_programming/0x18-dynamic_libraries/my_program.so
+./gm 9 8 10 24 75 9
--- 0x18-dynamic_libraries/my_program.c
+++ 0x18-dynamic_libraries/my_program.c
@@ -9,11 +9,10 @@
  */
 int main(void)
 {
-	/* Seed the random number generator */
-	srand(time(NULL));
-
 	int i, number;
 	int n1 = 9, n2 = 8, n3 = 10, n4 = 24, n5 = 75, bonus = 9;
+	/* Seed the random number generator */
+	srand(time(NULL));
 
 	/* Generate five numbers from 1 to 75 */
 	for (i = 1; i <= 5; i++)
