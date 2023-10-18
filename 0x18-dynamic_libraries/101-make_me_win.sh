48fcecd5935af7b1257ff23cb857fcb8ea1d6c3e
ahmed-my <yahaya.mymohammed@gmail.com>
2023-10-18 19:37:01 +0000

    Dynamic Library

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-wget -P /tmp https://github.com/ahmed-my/alx-low_level_programming/raw/master/0x18-dynamic_libraries/program.so
 export LD_PRELOAD=/home/vagrant/alx-low_level_programming/0x18-dynamic_libraries/program.so
